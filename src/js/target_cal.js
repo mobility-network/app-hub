b21458fc121d77a0ba96a3f0af1d28a2c0660898
Abdullah Zafar <42284331+abdulzaf@users.noreply.github.com>
2023-03-15 23:42:33 -0400

    Init

--- src/js/target_cal.js
+++
@@ -1,8 +0,0 @@
-inputCal.onchange = function() {
-    if (this.value==="" || this.value==="0") {
-        this.value = lengthPX;
-    }
-    lengthCM = this.value;
-    updateParamCM()
-    updateUILabels()
-}
--- src/js/trial_time.js
+++
@@ -1,6 +0,0 @@
-inputTime.onchange = function() {
-    if (this.value==="" || this.value<="0") {
-        this.value = 10;
-    }
-    timeLimitPlay = Math.round(this.value);
-}
